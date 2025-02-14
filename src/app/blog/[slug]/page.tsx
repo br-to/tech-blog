aa89b9721b5e75e5145df4446d1070ddf8f72d07
br-to <barabara0224@gmail.com>
2025-02-15 01:09:56 +0900

    ブログ記事詳細取得まで

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,54 +1,55 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "github-markdown-css";
+import { notFound } from "next/navigation";
 import styles from "./page.module.css";
 
-export default function Page() {
-	const markdown = `
-  # 見出し1
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
-  これはreact-markdownのサンプルです。
+export default async function Page({
+	params,
+}: { params: Promise<{ slug: string }> }) {
+	const { slug } = await params;
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${slug}`,
+		{
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		},
+	);
 
-  ## 見出し2
+	if (!res.ok) {
+		notFound();
+	}
 
-  - リスト1
-  - リスト2
-
-  \`\`\`javascript
-  const message = "Hello, world!";
-  console.log(message);
-  \`\`\`
-
-  **太字**
-
-  [リンク](https://www.google.com/)
-
-  > 引用
-
-  ## GFMの機能
-
-  | ヘッダー1 | ヘッダー2 |
-  |---|---|
-  | セル1 | セル2 |
-
-  ~~取り消し線~~
-
-  - [x] タスク完了
-  - [ ] タスク未完了
-
-  [^1]: 脚注
-
-   脚注付きテキスト[^1].
-    `;
+	const blogContent = await res.json();
 
 	return (
 		<div className={styles["blog-detail-page"]}>
 			<main className={styles.main}>
-				<h1 className={styles.title}>記事テスト</h1>
+				<h1 className={styles.title}>{blogContent.title}</h1>
 				<p className={styles.date}>Monday, February 25, 2019 · 6 min read</p>
-				<Markdown remarkPlugins={[remarkGfm]} className="markdown-body">
-					{markdown}
-				</Markdown>
+				{blogContent.mdblocks.map((mdblock: any) => {
+					const formattedMarkdown = mdblock.parent.replace(/\n/g, "  \n");
+					return (
+						<div className={styles.blocks} key={mdblock.blockId}>
+							<Markdown
+								remarkPlugins={[remarkGfm]}
+								components={{
+									ol: ({ node, ...props }) => (
+										<ol className={styles.ol} {...props} />
+									),
+									li: ({ node, ...props }) => <li {...props} />,
+								}}
+							>
+								{formattedMarkdown}
+							</Markdown>
+						</div>
+					);
+				})}
 			</main>
 		</div>
 	);
