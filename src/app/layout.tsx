1e08b507c98d6592a2f8024b7ef8952e59d1069c
br-to <barabara0224@gmail.com>
2025-02-16 23:31:33 +0900

    ブログTOPページのogp画像追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,17 @@ import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
 	title: "br-to Devlog",
 	description: "This blog is br-to's devlog.",
+	openGraph: {
+		title: "br-to Devlog",
+		description: "This blog is br-to's devlog.",
+		images: [
+			{
+				url: `${process.env.NEXT_PUBLIC_BASE_URL}/ogp.jpg`,
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
 };
 
 export default function RootLayout({
