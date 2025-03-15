bedaded16d99f6f4735987d07c5ae479b4d6c973
br-to <barabara0224@gmail.com>
2025-03-16 04:07:46 +0900

    notionのs3をremotePatternsから削除

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
-			},
-			{
-				protocol: "https",
 				hostname: "res.cloudinary.com",
 			},
 		],
