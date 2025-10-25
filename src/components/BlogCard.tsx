eb7c0bbe757cc3300d5bb3ccfcac9fe1fa851028
br-to <barabara0224@gmail.com>
2025-10-25 17:49:18 +0900

    feat: ブログカードコンポーネントに絵文字を追加し、スタイルを改善

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,51 +1,42 @@
-import { format } from "date-fns";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./BlogCard.module.css";
-import { Chip } from "./Chip";
+import { format } from 'date-fns';
+import Link from 'next/link';
+import styles from './BlogCard.module.css';
+import { Chip } from './Chip';
 
 export type BlogPost = {
-	id: string;
-	title: string;
-	publishedAt: string;
-	contentTypes: string[];
-	mainImage: string;
-	slug: string;
+  id: string;
+  title: string;
+  publishedAt: string;
+  contentTypes: string[];
+  emoji: string;
+  slug: string;
 };
 
 type BlogCardProps = {
-	post: BlogPost;
+  post: BlogPost;
 };
 
 export function BlogCard({ post }: BlogCardProps) {
-	return (
-		<article className={styles["blog-card"]}>
-			<Link href={`/blog/${post.slug}`}>
-				<div className={styles["image-container"]}>
-					<Image
-						src={post.mainImage || "/test.jpg"}
-						alt={post.title}
-						fill
-						className={styles.image}
-						sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
-					/>
-				</div>
-			</Link>
-			<div className={styles.content}>
-				{post.contentTypes && post.contentTypes.length > 0 && (
-					<div className={styles.contentTypes}>
-						{post.contentTypes.map((text) => (
-							<Chip text={text} key={text} />
-						))}
-					</div>
-				)}
-				<p className={styles.date}>{format(post.publishedAt, "yyyy/MM/dd")}</p>
-				<Link href={`/blog/${post.slug}`}>
-					<h2 className={styles.title}>{post.title}</h2>
-				</Link>
-			</div>
-		</article>
-	);
+  return (
+    <article className={styles['blog-card']}>
+      <Link href={`/blog/${post.slug}`}>
+        <div className={styles['emoji-container']}>
+          <span className={styles.emoji}>{post.emoji}</span>
+        </div>
+      </Link>
+      <div className={styles.content}>
+        {post.contentTypes && post.contentTypes.length > 0 && (
+          <div className={styles.contentTypes}>
+            {post.contentTypes.map((text) => (
+              <Chip text={text} key={text} />
+            ))}
+          </div>
+        )}
+        <p className={styles.date}>{format(post.publishedAt, 'yyyy/MM/dd')}</p>
+        <Link href={`/blog/${post.slug}`}>
+          <h2 className={styles.title}>{post.title}</h2>
+        </Link>
+      </div>
+    </article>
+  );
 }
