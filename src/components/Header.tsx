97938daa3e1176d9d0e51b34adf884c7830babaf
br-to <barabara0224@gmail.com>
2025-10-25 16:40:07 +0900

    style: ヘッダーコンポーネントのスタイルを修正し、SNSリンクを削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,24 @@
-import Image from "next/image";
-import Link from "next/link";
-import { EXTERNAL_URLS } from "@/constants/externalUrls";
-import styles from "./Header.module.css";
+import Link from 'next/link';
+import styles from './Header.module.css';
 
 export function Header() {
-	return (
-		<header className={styles.header}>
-			<div className={styles.container}>
-				<nav className={styles.nav}>
-					<Link href="/" className={styles.logo}>
-						br-to DevLog
-					</Link>
-
-					<div className={styles.sns}>
-						<Link
-							href={EXTERNAL_URLS.GITHUB}
-							className={styles.item}
-							target="_blank"
-						>
-							<Image src="/github.png" width={20} height={20} alt="github" />
-						</Link>
-						<Link
-							href={EXTERNAL_URLS.LINKDEIN}
-							className={styles.item}
-							target="_blank"
-						>
-							<Image
-								src="/linkedin.png"
-								width={20}
-								height={20}
-								alt="Linkedin"
-							/>
-						</Link>
-						<Link href="/rss.xml" className={styles.item}>
-							<Image src="/rss.svg" width={20} height={20} alt="RSS" />
-						</Link>
-					</div>
-				</nav>
-			</div>
-		</header>
-	);
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <nav className={styles.nav}>
+          <Link href="/" className={styles.logo}>
+            br-to.dev
+          </Link>
+          <div className={styles.navLinks}>
+            <Link href="/" className={styles.navLink}>
+              Home
+            </Link>
+            <Link href="/blogs" className={styles.navLink}>
+              Blog
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
